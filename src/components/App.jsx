0c591bfe2c7a34a44b8671f226131b80b7ff4fd3
Manish Kumar Nayak <dynamicmanish902@gmail.com>
2023-08-10 10:07:57 +0530

    UPDATE APP.JSX - ADD FUNCTIONALITY

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
-  });
+  const [item, setItem] = React.useState("");
 
-  function handleChange(event) {
-    const { name, value } = event.target;
+  const [taskList, setTaskList] = React.useState([]);
 
-    setContact(prevValue => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value
-        };
-      }
+  function handleItems(event) {
+    const newItem = event.target.value;
+    setItem(newItem);
+  }
+
+  function addTasks() {
+    setTaskList((task) => {
+      return [...taskList, item];
     });
+    setItem("");
   }
 
   return (
     <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input
-          onChange={handleChange}
-          name="fName"
-          value={contact.fName}
-          placeholder="First Name"
-        />
-        <input
-          onChange={handleChange}
-          name="lName"
-          value={contact.lName}
-          placeholder="Last Name"
-        />
-        <input
-          onChange={handleChange}
-          name="email"
-          value={contact.email}
-          placeholder="Email"
-        />
-        <button>Submit</button>
-      </form>
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleItems} value={item} />
+        <button onClick={addTasks}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {taskList.map((addedItem) => {
+            return <li>{addedItem}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
